4c6e354a6f483ccb740e128dcb1c45ddeea48ff8
FernandoLorca <florcapinto@gmail.com>
2023-09-07 23:41:51 -0300

    Add overflow hidden and break word to NoteCardTitle component to control the one word too big titles

--- src/components/NotesPage/NoteCardTitle.jsx
+++ src/components/NotesPage/NoteCardTitle.jsx
@@ -1,7 +1,7 @@
 export default function NoteCardTitle({ title }) {
   return (
-    <p className="text-md font-bold mr-10">
-      <span className="text-lg">🗒️ {title}</span>
+    <p className="text-md font-bold mr-10 overflow-hidden">
+      <span className="text-lg break-words">🗒️ {title}</span>
     </p>
   );
 }
